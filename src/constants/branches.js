ed5f0cb82522aad3fa3a4de6ca33840cbd02a947
FahadNadeem560 <fahad-nadeem@hotmail.com>
2026-05-30 17:25:27 +0500

    Refactor: add branch constants

---
+++ src/constants/branches.js
@@ -0,0 +1,17 @@
+export const BRANCH_CODE_MAP = {
+  "HO - Admin": "HO",
+  "Main Branch": "MAIN",
+  "DHA Branch": "DHA",
+  "WAREHOUSE": "WH",
+  "BASE FAISAL": "PAF",
+};
+
+export const BRANCH_ALIASES = {
+  "ho - admin": "HO - Admin",
+  "ho-admin": "HO - Admin",
+  "main branch": "Main Branch",
+  "dha branch": "DHA Branch",
+  "warehouse": "WAREHOUSE",
+  "base faisal": "BASE FAISAL",
+  "paf faisal": "BASE FAISAL",
+};
